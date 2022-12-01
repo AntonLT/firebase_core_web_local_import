33bae9d82fc635ea57f75df5316735177a466433
Anton Muntianu <antonlteacup@gmail.com>
2022-12-01 12:19:16 +0200

    update purpose

--- packages/firebase_ui_localizations/lib/firebase_ui_localizations.dart
+++
@@ -1,5 +0,0 @@
-export 'src/l10n.dart'
-    show FirebaseUILocalizations, FirebaseUILocalizationDelegate;
-
-export 'src/default_localizations.dart'
-    show DefaultLocalizations, FirebaseUILocalizationLabels;
